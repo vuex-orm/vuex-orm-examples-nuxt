3395e3bf8b6144a22167e392de8eaa692ad69aff
Simon Schrape <sim@ilitu.de>
2019-02-26 15:28:21 +0100

    Use store in modules mode. See https://nuxtjs.org/guide/vuex-store#plugins Classic mode is deprecated (https://nuxtjs.org/guide/vuex-store#classic-mode).

--- store/index.js
+++ store/index.js
@@ -1,14 +1,7 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
 import VuexORM from '@vuex-orm/core'
 import database from './database'
 
-Vue.use(Vuex)
+export const plugins = [ VuexORM.install(database) ]
 
-function createStore () {
-  return new Vuex.Store({
-    plugins: [VuexORM.install(database)]
-  })
-}
-
-export default createStore
+export const state = () => ({
+})
