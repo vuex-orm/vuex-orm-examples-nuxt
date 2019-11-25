3cb4c887f2e158341f83da7ba890e7b81194e327
Kia Ishii <kia.king.08@gmail.com>
2019-11-25 20:24:19 +0900

    Update Vuex ORM to the latest version

--- data/index.js
+++ data/index.js
@@ -3,25 +3,19 @@
 export default async function data () {
   return [
     {
-      id: 1,
-      user_id: 1,
       title: 'Create awesome application!',
       done: false,
-      assignee: { id: 1, name: 'John Doe' }
+      assignee: { name: 'John Doe' }
     },
     {
-      id: 2,
-      user_id: 1,
       title: 'Read the documentation',
       done: false,
-      assignee: { id: 1, name: 'John Doe' }
+      assignee: { name: 'John Doe' }
     },
     {
-      id: 3,
-      user_id: 2,
       title: 'Star Vuex ORM repository',
       done: false,
-      assignee: { id: 2, name: 'Jane Doe' }
+      assignee: { name: 'Johnny Doe' }
     }
   ]
 }
